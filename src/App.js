5db46af447bae31ab396baea8a870046e8c8d225
ironsummit72 <ironsumit72@gmail.com>
2023-11-03 13:14:44 +0530

    sort by name bug fixed

--- src/App.js
+++ src/App.js
@@ -1,106 +1,89 @@
-
 import { useEffect, useState } from "react";
 import Card from "./components/Card";
 import Card2 from "./components/Card2";
 import Navbar from "./components/Navbar";
 import Filter from "./components/Filter";
-import "./style/filter.css"
+import axios from "axios"
+import "./style/filter.css";
 function App() {
-  const [products,setProducts]=useState([]);
-  const [sortByPrice,setSortByPrice]=useState([]);
-  const [sortByName,setSortByName]=useState([]);
-  const isLocal=false;
-  let filterValue=undefined
-  const BackendUrl= isLocal? 'http://localhost:3000/':'http://192.168.1.100:3000/'
-  const byPriceSort= (products)=>{
-    
-    let sorted= products.map(element => {
-      return element
-    });
-    let byPrice= sorted.sort((a,b)=> b.ProductPrice-a.ProductPrice)
-    return byPrice
-    //setProducts(sortByPrice)
-    
-    
-  }
-  const byNameSort=(products)=>{
-    let sorted= products.map(element => {
-      return element
-    });
-    let byName= sorted.sort((a,b)=>{
-      let fa=a.ProductName.toLowerCase()
-      let fb=b.ProductName.toLowerCase()
-      if(fa<fb)
-      {
-        return -1
-      }
-      if(fa>fb)
-      {
-        return 0
-      }
-      return 0;
-    })
-  console.log(byName);
-  setSortByName(byName)
-  setProducts(sortByName)
+  const [card, setCard] = useState([]);
+  const [responseState, setResponse] = useState([]);
+  const isLocal = false;
+  const BackendUrl = isLocal
+    ? process.env.REACT_APP_LOCAL_URL
+    : process.env.REACT_APP_BACKEND_URL;
 
-  
+const sortByName=(data)=>{
+const newarray=data.map((items)=>items)
+let sort=newarray.sort((a,b)=>{
+  let fa=a.ProductName.toLowerCase()
+  let fb=b.ProductName.toLowerCase()
+  if(fa<fb)
+  {
+    return -1
+  }
+  if(fa>fb)
+  {
+    return 0
+  }
+  return 0
+})
+return sort;
+}
+
+
+const sortByPrice=(data)=>{
+  const newarray= data.map((items)=>items)
+
+}
+
+async function getData()
+{
+  try {
+    const response=await axios.get(BackendUrl)
+    setResponse(response.data)
+    setCard(response.data)
+
+   
+  }catch (error)
+  {
+    console.log(error);
+  }
 }
 useEffect(()=>{
-  console.log("on start");
-  fetchUserData()
-
+  getData()
 },[])
 
-const filterOnSelect=(e)=>{
-   filterValue=e.target.value
-  if(filterValue==='price')
-  {
-    let Price=byPriceSort(products)
-  
-    setSortByPrice(Price)
-    setProducts(sortByPrice)
-      
-      
-    }if(filterValue==='name')
-    {
-      byNameSort(products)
-      
+
+
+    const onFilterChange=(e)=>{
+      const value=e.target.value
+    
+      if (value==='default')
+      {
+        setCard(responseState)
+      }else if(value==='name')
+      {
+         setCard(sortByName(responseState))
+      }else if(value==='price')
+      {
+        setCard(sortByPrice(responseState));
+      }else{
+        setCard(responseState)
+      }
     }
-   
 
-  }
-  useEffect(()=>{
-
-   byNameSort(products)
-  
-  },[filterValue])
-
-  const fetchUserData=async()=>{
-    console.log(BackendUrl);
-    let data=await fetch(`${BackendUrl}products`)
-    let response=await data.json()
-    console.log(response);
-    setProducts(response);  
-  }
-  
- // console.log(products[0].Paths[0].path); ${items.Paths[3].path}
- 
+  // console.log(products[0].Paths[0].path); ${items.Paths[3].path}
 
   return (
-<>
-<Navbar/>
-<Filter onHandleChange={filterOnSelect}/>
-{
-  products.map((items,index)=>{
-    return <Card2 productname={items.ProductName} price={items.ProductPrice} imageurl={`${BackendUrl}${items.Paths[3].path}`} onHandleClick={(e)=>{console.log(e.target.firstChild.innerText,`_id ${items._id}`);
-    document.title=e.target.firstChild.innerText
-    }}  key={items._id}  />
-  })
-}
-
-</>
-  )
+    <>
+      <Navbar />
+      <Filter onHandleChange={onFilterChange}/>
+      {card.map((items,key)=>{
+        return <Card key={key} productname={items.ProductName} price={items.ProductPrice} imageurl={`${BackendUrl}${items.Paths[0].path}`} onHandleClick={()=>{}}/>
+      })}
+    </>
+  );
 }
 
 export default App;
