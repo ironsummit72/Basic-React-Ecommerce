cad1f3da3f5bd1eb74e4c190b057f299fb2a4e93
ironsummit72 <ironsumit72@gmail.com>
2023-11-03 22:56:46 +0530

    productView shifted to pages

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import {
   Route,
   Routes,
 } from "react-router-dom";
-import Productview from "./components/Productview";
+import Productview from "./pages/Productview";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
