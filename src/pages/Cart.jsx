b57c4cf053618e724e1b863e569f21ea910f2aaa
ironsummit72 <ironsumit72@gmail.com>
2023-11-04 15:39:07 +0530

    items are now visable in cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { getLocalStorageData } from "../module/LocalStorageApi";
+import Card from "../components/Card";
 
-function Cart(productName,productPrice,imgUrl) {
+function Cart() {
+  const isLocal = false;
+  const BackendUrl = isLocal
+    ? process.env.REACT_APP_LOCAL_URL
+    : process.env.REACT_APP_BACKEND_URL;
+  const [resposnseData, setResponseData] = useState([]);
+
+  const localstorageData = JSON.stringify(getLocalStorageData("cartData"));
+  const getDataFromDatabase = async () => {
+    let response = await axios.get(
+      `${BackendUrl + "cartData?cartitems=" + localstorageData}`
+    );
+    setResponseData(response.data);
+  };
+
+  useEffect(() => {
+    getDataFromDatabase();
+  }, []);
+
   return (
     <>
-<h1>cart</h1>
+      {resposnseData.map((data) => {
+        return (
+          <Card
+            productname={data.ProductName}
+            price={data.ProductPrice}
+            productid={data._id}
+            imageurl={BackendUrl + data.Paths[0].path}
+          />
+        );
+      })}
     </>
-  )
+  );
 }
 
-export default Cart
+export default Cart;
