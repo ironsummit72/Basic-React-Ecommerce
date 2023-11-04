0ed50237e1bb78d5c6ecef152a9b2c26858f0e64
ironsummit72 <ironsumit72@gmail.com>
2023-11-04 14:07:38 +0530

    added this component for better code readibility and structuing of router-dom

---
+++ src/Main.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { BrowserRouter,Routes,Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Productview from './pages/Productview'
+import Cart from './pages/Cart'
+import App from './App'
+import { Context } from './context/MyContext'
+import { getLocalStorageDataLength } from './module/LocalStorageApi'
+
+function Main() {
+const [cartCount,setCartCount]=useState(getLocalStorageDataLength('cartData'));
+document.addEventListener('localdatachanged',()=>{
+    setCartCount(getLocalStorageDataLength('cartData'))
+});
+    return (
+        <>
+      <BrowserRouter>
+      <Context.Provider value={{cartCount}}>
+      <Navbar/>
+      </Context.Provider>
+    <Routes>
+      <Route path="/productview/:id" element={<Productview/>} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+    </>
+  )
+}
+
+export default Main
