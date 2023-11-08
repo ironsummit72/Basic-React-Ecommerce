2daed727e0b57e0e9fcdc325ee8bd1011c2bbf5f
ironsummit72 <ironsumit72@gmail.com>
2023-11-09 00:55:10 +0530

    added number of reviews

--- src/components/DefaultRating.jsx
+++ src/components/DefaultRating.jsx
@@ -1,6 +1,10 @@
 import { Rating } from "@material-tailwind/react";
  
-export function DefaultRating({rating,className}) {
+export function DefaultRating({rating,className,numberOfReviews=98}) {
 //   return <Rating value={rating} />;
-return <Rating unratedColor="red" ratedColor="red" value={rating} className={className} readonly />
+return <div className="container w-60 flex items-center justify-between "> 
+<Rating unratedColor="red" ratedColor="red" value={rating} className={className} readonly />
+<h1 className="mt-6">{numberOfReviews} reviews</h1>
+</div> 
+
 }
