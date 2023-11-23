8c94ab9cdb7a241864ddbc8f015a1dce90f0544e
ironsummit72 <ironsumit72@gmail.com>
2023-11-23 22:18:32 +0530

    hides the next and previous buttons when last item and first item has reached

--- src/components/ImageView.jsx
+++ src/components/ImageView.jsx
@@ -6,24 +6,35 @@ import { IoMdClose } from "react-icons/io";
 
 function ImageView({indexImg,imgarray,onClose,BackendUrl}) {
         const [index, setIndex] = useState(indexImg);
+        const [isNext,setIsNext] = useState(index < imgarray.length - 1?true:false);
+        const [isPrevious,setIsPrevious] = useState(index > 0?true:false);
+        
         const onNext = () => {
           if (index < imgarray.length - 1) {
+            setIsPrevious(true)
             setIndex(index + 1);
+          }
+          else{
+            setIsNext(false);
           }
         };
         const onPrevious = () => {
           if (index > 0) {
+            setIsNext(true)
             setIndex(index - 1);
+          }
+          else{
+            setIsPrevious(false)
           }
         };
 
         return (
             <div className="w-full h-full flex flex-col items-center mt-8">
                 <img src={BackendUrl+imgarray[index].path} className="w-2/4 object-cover"  alt="" />
-                <div className="btns flex justify-between w-96 sticky bottom-4">
-                <button className='w-20 h-20 border-2 bg-white rounded-full flex justify-center items-center' onClick={onPrevious}><GrFormPrevious /></button>
+                <div className="flex justify-between w-96 sticky bottom-4">
+               <button className={`${isPrevious?"":"hidden"} w-20 h-20 border-2 bg-white rounded-full flex justify-center items-center`} onClick={onPrevious}><GrFormPrevious /></button>
                 <button className='w-20 h-20 border-2 bg-white rounded-full flex justify-center items-center' onClick={onClose}><IoMdClose /></button>
-                <button className='w-20 h-20 border-2 bg-white rounded-full flex justify-center items-center' onClick={onNext}><MdNavigateNext/></button>
+                <button className={`${isNext?"":"hidden"} w-20 h-20 border-2 bg-white rounded-full flex justify-center items-center`} onClick={onNext}><MdNavigateNext/></button>
                 </div>
             </div>
         )
