81def1ca9dfc2fb0f8287e7e7f67a5e234cb2865
ironsummit72 <ironsumit72@gmail.com>
2023-11-23 21:51:29 +0530

    Removed profile icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,10 +45,6 @@ function Navbar()
           </div>
         </div>
       </div>
-      <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-        <button type="button" className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-        </button>
-      </div>
     </div>
   </div>
   <div className="sm:hidden" id="mobile-menu">
